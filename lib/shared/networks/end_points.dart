b7b1366155ffa1037f394bca471cecaaceba2cc6
MagdKamaldev <magddevf@gmail.com>
2023-08-21 09:40:20 +0300

    constants

--- lib/shared/networks/end_points.dart
+++ lib/shared/networks/end_points.dart
@@ -7,5 +7,10 @@ const updateProfile = "update-profile";
 const searchUrl = "products/search";
 const carts = "carts";
 
+//https://accept.paymob.com/api/auth/tokens
 
-//AIzaSyCrGaeGRcH5blcSkHYiqKsEUVhee_jf1X0
+class PayMobConst {
+  static const String paymobBaseUrl = "https://accept.paymob.com/api";
+  static const String getAuthToken = "/auth/tokens";
+  static const String paymentApiKey = "ZXlKaGJHY2lPaUpJVXpVeE1pSXNJblI1Y0NJNklrcFhWQ0o5LmV5SmpiR0Z6Y3lJNklrMWxjbU5vWVc1MElpd2libUZ0WlNJNkltbHVhWFJwWVd3aUxDSndjbTltYVd4bFgzQnJJam8zTURJMU5EQjkuenpjd3hZY2I2ak5rcHlTNENWVnFjdUhxc0F5LU5iYXc2Z2lGZEFpTXdBeGgwSUdmdDJYdGUwN2FOLTZWSEtDdHV1cm1id3pvSHRoUXVkMFJsUDVENXc=";
+}
